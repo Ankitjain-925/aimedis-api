a6a498ebdbb62749ebf4405fdf7a3e466a4ce294
ruchi1234 <info.webnexus@gmail.com>
2022-03-21 11:01:38 +0530

    update in virtual_task

--- schema/virtual_tasks.js
+++ schema/virtual_tasks.js
@@ -4,51 +4,52 @@ var Schema = mongoose.Schema;
 const mongooseFieldEncryption = require("mongoose-field-encryption").fieldEncryption;
 
 const ProfessionalInfo = new mongoose.Schema({
-    image:{
+    image: {
         type: String,
         required: false,
         unique: false
     },
-    name:{
+    name: {
         type: String,
         required: false,
         unique: false
     },
-    profile_id:{
+    profile_id: {
         type: String,
         required: false,
         unique: false
     },
-    alies_id:{
+    alies_id: {
         type: String,
         required: false,
         unique: false
     }
-},{ strict: false });
+}, { strict: false });
 
 ProfessionalInfo.plugin(mongooseFieldEncryption, {
     fields: ["name", "image", "alies_id"],
     secret: process.env.SOME_32BYTE_BASE64_STRING,
     saltGenerator: function (secret) {
         return "1234567890123456"; // should ideally use the secret to return a string of length 16
-} });
+    }
+});
 
 var TaskSchema = new Schema({
     assinged_to: [ProfessionalInfo],
-    task_name:{
+    task_name: {
         type: String,
         required: true,
         unique: false
     },
-    description:{
-      type: String,
-      required: false,
-      unique: false
+    description: {
+        type: String,
+        required: false,
+        unique: false
     },
     attachments: {
-        type : Array,
+        type: Array,
         required: false,
-        unique: false 
+        unique: false
     },
     house_id: {
         type: String,
@@ -60,17 +61,17 @@ var TaskSchema = new Schema({
         required: false,
         unique: false
     },
-    comments:{
-        type : Array,
+    comments: {
+        type: Array,
         required: false,
-        unique: false 
+        unique: false
     },
     created_at: {
         type: String,
         required: false,
         unique: false
     },
-    status:{
+    status: {
         type: String,
         required: true,
         unique: false
@@ -86,21 +87,144 @@ var TaskSchema = new Schema({
         required: false,
         unique: false
     },
-    case_id:{
+    case_id: {
         type: String,
         required: false,
         unique: false
     },
-    done_on:{
+    done_on: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    bloodpressure: {
+        type: Object,
+        required: false,
+        unique: false
+    },
+    diabetes: {
+        type: Object,
+        required: false,
+        unique: false
+    },
+    smoking_status: {
+        type: Object,
+        required: false,
+        unique: false
+    },
+    allergies: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    family_history: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    treatment_so_far: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    place_of_birth: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    race: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    travel_history: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    medical_precondition: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    premedication: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    image_evaluation: {
+        type: Object,
+        required: false,
+        unique: false
+    },
+    start_date: {
+        type: Date,
+        required: false,
+        unique: false
+    },
+    warm: {
+        type: Boolean,
+        required: false,
+        unique: false
+    },
+    size_progress: {
+        type: Boolean,
+        required: false,
+        unique: false
+    },
+    itch: {
+        type: Boolean,
+        required: false,
+        unique: false
+    },
+    pain: {
+        type: Boolean,
+        required: false,
+        unique: false
+    },
+    pain_level: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    body_temp: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    sun_before: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    cold: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    sexual_activities: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    payment_data: {
+        type: Object,
+        required: false,
+        unique: false
+    },
+    Task_type:{
         type: String,
         required: false,
         unique: false
     }
-},{ strict: false });
 
-TaskSchema.plugin(mongooseFieldEncryption, { fields: [ "task_name", "description"], secret: process.env.SOME_32BYTE_BASE64_STRING,
-saltGenerator: function (secret) {
-    return "1234567890123456"; // should ideally use the secret to return a string of length 16
-  } });
+}, { strict: false });
+
+TaskSchema.plugin(mongooseFieldEncryption, {
+    fields: ["task_name", "description"], secret: process.env.SOME_32BYTE_BASE64_STRING,
+    saltGenerator: function (secret) {
+        return "1234567890123456"; // should ideally use the secret to return a string of length 16
+    }
+});
 var virtual_Task = mongoose.model('virtual_task', TaskSchema);
 module.exports = virtual_Task;
