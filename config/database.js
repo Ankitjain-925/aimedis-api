63fe99077b25645e254eb8399e581524a6a9df68
Ruchi-agrawal <ruchi.agarwal2005@gmail.com>
2022-06-17 17:34:28 +0530

    add port number

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-	'database': 'mongodb://localhost/aimedis_enc_demo'
+	'database': 'mongodb://localhost:27170/aimedis_testing'
 	// 'database': 'mongodb://85.214.252.27/aimedis'
 }
