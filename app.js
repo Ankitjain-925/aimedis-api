45c131ac9fd7790312b7abfbf245d3f6cf7591a9
ruchi1234 <info.webnexus@gmail.com>
2022-10-29 14:24:33 +0530

    sokcket work move

--- app.js
+++ app.js
@@ -56,74 +56,7 @@ appAdmin2.use(express.static(path.join(__dirname, "build/sickleave")));
 appAdmin3.use(express.static(path.join(__dirname, "build/videoConf")));
 app.use(express.static(path.join(__dirname, "build/main")));
 
-const server = require("http").createServer(app);
-const io = require("socket.io")(server, {
-  transports: ["polling","websocket"],
-  cors: {
-    origin: "*",
-   
-  },
- 
-});
 
-io.on("connection", (socket) => {
-  console.log("A user is connected");
-
-  socket.on("update", (data) => {
-    socket.broadcast.emit("data_shown", data);
-  });
-  socket.on("addpatient", (data) => {
-    socket.broadcast.emit("email_accept", data);
-  });
-  socket.on("decline", (data) => {
-    socket.broadcast.emit("email_decline", data);
-  });
-  socket.on('nurse',(data)=>{
-    socket.broadcast.emit("displaynurse", data);
-    // socket.destroy();
-  })
-  socket.on('doctor',(data)=>{
-    socket.broadcast.emit("displaydoctor", data);
-    // socket.destroy();
-  })
-  socket.on("delete",(data)=>{
-    socket.broadcast.emit("deletedata",data)
-    // socket.destroy();
-  })
-  socket.on("Updated",(data)=>{
-    socket.broadcast.emit("Updateddata",data)
-    // socket.destroy();
-  })
-
-  socket.on("deleteN",(data)=>{
-    socket.broadcast.emit("deletedataN",data)
-    // socket.destroy();
-  })
-  socket.on("UpdateN",(data)=>{
-    socket.broadcast.emit("UpdateddataN",data)
-    // socket.destroy();
-  })
-
-  socket.on("deleteA",(data)=>{
-    socket.broadcast.emit("deletedataA",data)
-    // socket.destroy();
-  })
-  socket.on("UpdateA",(data)=>{
-    socket.broadcast.emit("UpdateddataA",data)
-    // socket.destroy();
-  })
-
-  socket.on('adminstaff',(data)=>{
-    socket.broadcast.emit("displayadmin", data);
-    // socket.destroy();
-    // socket.disconnect()
-  })
-
-  // socket.on('disconnect', () => {
-  //   console.log(`socket disconnected`);
-
-  // })
-});
 ////////////admin+main+end/////////////
 
 // cron.schedule('0 0 */12 * * *', function(){
