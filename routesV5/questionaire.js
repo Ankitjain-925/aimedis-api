a1da58d60e2ae0081cd39d251af9c1ebc876b6e1
vaibhavwebnexus <vaibhav.webnexus@gmail.com>
2022-11-15 12:33:17 +0530

    Add role

--- routesV5/questionaire.js
+++ routesV5/questionaire.js
@@ -184,7 +184,7 @@ router.post("/AddQuestionaire", CheckRole("add_questionnaire"), function (req, r
     }
 });
 
-router.put("/Question/:questionaire_id", CheckRole("edit_questionnaire"), function (req, res, next) {
+router.put("/Question/:questionaire_id/:house_id", CheckRole("edit_questionnaire"), function (req, res, next) {
     const token = req.headers.token;
     let legit = jwtconfig.verify(token);
     if (legit) {
